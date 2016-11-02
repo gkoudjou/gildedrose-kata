71f1f3242c4fc8d06afd94f4593f4ea67313d130
Giovanny Temgoua <giovanny.koudjou@outlook.com>
2016-11-02 23:19:24 +0100

    Isolate rule for Conjured item

--- src/main/java/net/gtemgoua/kata/gildedrose/GildedRose.java
+++ src/main/java/net/gtemgoua/kata/gildedrose/GildedRose.java
@@ -6,6 +6,7 @@ class GildedRose {
     public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
 	public static final String BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
 	public static final String AGED_BRIE = "Aged Brie";
+	public static final String CONJURED = "Conjured";
 
 	Item[] items;
 
@@ -24,14 +25,32 @@ class GildedRose {
         	else if (items[i].name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
         		applyBackStagesRules(items[i]);
         	}
+        	else if (items[i].name.equals(CONJURED)) {
+        		applyConjuredRules(items[i]);
+        	}
         	else
         		applyNormalRules(items[i]);
         
         }
     }
     
-    private void applyNormalRules(Item item) {
-    	if(item.sellIn == 0)
+    private void applyConjuredRules(Item item) {
+    	if(item.sellIn <= 0)
+    		item.quality -= 4;
+    	else
+    		item.quality -= 2;
+    	
+    	if( item.quality > 50)
+    		item.quality = 50;
+    	
+    	if(item.quality < 0)
+    		item.quality = 0;
+    	
+    	item.sellIn--;
+	}
+
+	private void applyNormalRules(Item item) {
+    	if(item.sellIn <= 0)
     		item.quality -= 2;
     	else
     		item.quality--;
