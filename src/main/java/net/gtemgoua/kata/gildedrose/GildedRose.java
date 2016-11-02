9909d810c508e651ed02b4c094cd0d6bc761bd27
Giovanny Temgoua <giovanny.koudjou@outlook.com>
2016-11-02 22:48:12 +0100

    Fix Aged Brie rules as all the tests didn't pass

--- src/main/java/net/gtemgoua/kata/gildedrose/GildedRose.java
+++ src/main/java/net/gtemgoua/kata/gildedrose/GildedRose.java
@@ -28,10 +28,13 @@ class GildedRose {
     }
     
     private void applyAgedBrieRules(Item item) {
-    	if(item.sellIn < 0)
+    	if(item.sellIn <= 0)
     		item.quality += 2;
     	else
     		item.quality++;
+    	
+    	if( item.quality > 50)
+    		item.quality = 50;
     	
     	item.sellIn--;
 	}
