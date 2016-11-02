e7d34e4fc07db94d6b84647920f612902a7c49f0
Giovanny Temgoua <giovanny.koudjou@outlook.com>
2016-11-02 23:25:05 +0100

    Update Conjured constant to fix the TexttestFixture class

--- src/main/java/net/gtemgoua/kata/gildedrose/GildedRose.java
+++ src/main/java/net/gtemgoua/kata/gildedrose/GildedRose.java
@@ -6,7 +6,7 @@ class GildedRose {
     public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
 	public static final String BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
 	public static final String AGED_BRIE = "Aged Brie";
-	public static final String CONJURED = "Conjured";
+	public static final String CONJURED = "Conjured Mana Cake";
 
 	Item[] items;
 
